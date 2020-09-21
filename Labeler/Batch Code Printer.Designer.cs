0f48e03e67021492210df1f5128a213439d0cd62
wkershaw <wkershaw456@gmail.com>
2020-09-21 16:16:20 +0100

    Add project files.

---
+++ Labeler/Batch Code Printer.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Labeler
+{
+    partial class BatchCodeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BatchCodeForm));
+            this.CreateButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BatchCode = new System.Windows.Forms.TextBox();
+            this.StartingNumber = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.NumberOfBottles = new System.Windows.Forms.NumericUpDown();
+            this.Prefix = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.LabelSize = new System.Windows.Forms.ComboBox();
+            this.ConnectionLabel = new System.Windows.Forms.Label();
+            this.CancelButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.StartingNumber)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumberOfBottles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CreateButton
+            // 
+            resources.ApplyResources(this.CreateButton, "CreateButton");
+            this.CreateButton.Name = "CreateButton";
+            this.CreateButton.UseVisualStyleBackColor = true;
+            this.CreateButton.Click += new System.EventHandler(this.CreateButton_ClickAsync);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // BatchCode
+            // 
+            resources.ApplyResources(this.BatchCode, "BatchCode");
+            this.BatchCode.Name = "BatchCode";
+            // 
+            // StartingNumber
+            // 
+            resources.ApplyResources(this.StartingNumber, "StartingNumber");
+            this.StartingNumber.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.StartingNumber.Name = "StartingNumber";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // NumberOfBottles
+            // 
+            resources.ApplyResources(this.NumberOfBottles, "NumberOfBottles");
+            this.NumberOfBottles.Maximum = new decimal(new int[] {
+            512,
+            0,
+            0,
+            0});
+            this.NumberOfBottles.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.NumberOfBottles.Name = "NumberOfBottles";
+            this.NumberOfBottles.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Prefix
+            // 
+            resources.ApplyResources(this.Prefix, "Prefix");
+            this.Prefix.Name = "Prefix";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // LabelSize
+            // 
+            this.LabelSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            resources.ApplyResources(this.LabelSize, "LabelSize");
+            this.LabelSize.FormattingEnabled = true;
+            this.LabelSize.Name = "LabelSize";
+            // 
+            // ConnectionLabel
+            // 
+            resources.ApplyResources(this.ConnectionLabel, "ConnectionLabel");
+            this.ConnectionLabel.Name = "ConnectionLabel";
+            // 
+            // CancelButton
+            // 
+            resources.ApplyResources(this.CancelButton, "CancelButton");
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.TabStop = false;
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // BatchCodeForm
+            // 
+            this.AcceptButton = this.CreateButton;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.ConnectionLabel);
+            this.Controls.Add(this.LabelSize);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Prefix);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.NumberOfBottles);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.StartingNumber);
+            this.Controls.Add(this.BatchCode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CreateButton);
+            this.Name = "BatchCodeForm";
+            ((System.ComponentModel.ISupportInitialize)(this.StartingNumber)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumberOfBottles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CreateButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox BatchCode;
+        private System.Windows.Forms.NumericUpDown StartingNumber;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown NumberOfBottles;
+        private System.Windows.Forms.TextBox Prefix;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox LabelSize;
+        private System.Windows.Forms.Label ConnectionLabel;
+        private System.Windows.Forms.Button CancelButton;
+    }
+}
+
---
+++ Labeler/Batch Code Printer.cs
@@ -0,0 +1,185 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Zebra.Sdk.Comm;
+using Zebra.Sdk.Printer;
+using Zebra.Sdk.Printer.Discovery;
+using Task = System.Threading.Tasks.Task;
+
+namespace Labeler
+{
+    public partial class BatchCodeForm : Form
+    {
+        private struct LabelSetup
+        {
+            public int pagewidth;
+            public string fontsize;
+            public int offset;
+            public string batchcode;
+            public string prefix;
+            public int startNumber;
+            public int quantity;
+        }
+
+        ZebraPrinter printer;
+        bool cancel = false;
+
+        public BatchCodeForm()
+        {
+            InitializeComponent();
+
+            //Set Label Size Options
+            //Small: 158 points wide
+            //Large: 235 points wide
+            LabelSize.Items.AddRange(
+                new object[]
+                {
+                    "Small (2cm x 1.2cm)",
+                    "Large (3cm x 2cm)"
+                });
+            LabelSize.SelectedIndex = 1;
+            
+            
+            //Try to connect to the printer
+            try
+            {
+                //Connect to first availaible printer
+                ConnectToPrinter();
+            }
+            catch (Exception error)
+            {
+                ConnectionLabel.Text = "Printer cannot be found, close the program and try again :" + error.Message; 
+                CreateButton.Enabled = false;
+            }
+        }
+
+        private async void CreateButton_ClickAsync(object sender, EventArgs e)
+        {
+            CreateButton.Enabled = false;
+            cancel = false;
+
+            //Check that the printer is ready
+            if (!PrinterIsReady())
+            {
+                ConnectionLabel.Text = "Printer is not ready to print, close the program and try again";
+                return;
+            }
+
+            //Create a new LabelSetup and add information from the form
+            LabelSetup l;
+            l.batchcode = BatchCode.Text;
+            l.startNumber = (int)StartingNumber.Value;
+            l.quantity = (int)NumberOfBottles.Value;
+            l.prefix = Prefix.Text;
+
+            //Change the page width and font size based on label size
+            if (LabelSize.SelectedIndex == 0)
+            {
+                l.pagewidth = 158;
+                l.fontsize = "43,18";
+                l.offset = 20;
+            }
+            else
+            {
+                l.pagewidth = 235;
+                l.fontsize = "45,20";
+                l.offset = 40;
+            }
+
+            //Print the labels using the label setup
+            await Task.Factory.StartNew(() =>
+                {
+                    PrintAndWait(l);
+                }
+            );
+
+            cancel = false;
+            CreateButton.Enabled = true;
+
+        }
+
+        private void PrintAndWait(LabelSetup l, int time = 3000)
+        {
+            PrintLabels(l);
+            Thread.Sleep(time);
+            while (!PrinterIsReady());
+        }
+
+        private void PrintLabels(LabelSetup l)
+        {
+            //Define the label format used to layout the label
+            string labelFormat =
+                $"^XA" +                                // Start the format
+                $"^DFE:LABEL.ZPL^FS" +                  // Set the name of the format file
+                $"^PW{l.pagewidth}" +                     // Set the pagewidth
+                $"^A@N,{l.fontsize},E:TT0003M_.FNT" +     // Set the font and fontsize
+                $"^FO0,{l.offset}" +                      // Set the starting position using the offset
+                $"^FB{l.pagewidth},2,0,C" +               // Create a field block to center the text, max 2 lines
+                $"^FN1\"Batch Code Text\"^FS" +         // Define the label text into variable slot 1
+                $"^XZ";                                 // End the format
+
+            //Send the label format to the printer
+            printer.SendCommand(labelFormat);
+
+
+            //Initialise a dictionary to pass variables to the printer
+            Dictionary<int, string> vars = new Dictionary<int, string> { };
+
+            //Print each label individually, providing a string for the label text via the dictionary
+            for (int i = 0; i < l.quantity; i++)
+            {
+                if (cancel)
+                {
+                    break;
+                }
+                //Change the text based on the size of the label
+                if (l.pagewidth < 200)
+                {
+                    vars[1] = $"{l.batchcode}\\&";
+                    vars[1] += $"{l.prefix}{l.startNumber + i}\\&";
+                }
+                else
+                {
+                    vars[1] = $"{l.batchcode}\\&";
+                    vars[1] += $"Bottle: {l.prefix}{l.startNumber + i}\\&";
+                }
+
+                //Print the label with associated text
+                printer.PrintStoredFormat("E:LABEL.ZPL", vars);
+            }
+
+
+        }
+        
+        private void ConnectToPrinter()
+        {
+            DiscoveredUsbPrinter discoveredPrinter = UsbDiscoverer.GetZebraUsbPrinters(new ZebraPrinterFilter())[0];
+            Connection connection = discoveredPrinter.GetConnection();
+            connection.Open();
+            printer = ZebraPrinterFactory.GetInstance(connection);
+            ConnectionLabel.Text = "Printer Connected!";
+        }
+        
+        private bool PrinterIsReady()
+        {
+            bool ready;
+            try
+            {
+                ready = printer != null && printer.GetCurrentStatus().isReadyToPrint;
+            }catch (Exception)
+            {
+                ready = false;
+            }
+            return ready;
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            cancel = true;
+        }
+    }
+}
